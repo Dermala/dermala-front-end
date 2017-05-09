67de96b52c3e325c5308a9b75a804b0f7099fdca
Sabrina Simeroth <ses0607@gmail.com>
2017-05-09 15:55:22 -0700

    merge changes

--- screens/PhotoCaptureScreen.js
+++ screens/PhotoCaptureScreen.js
@@ -1,60 +1,24 @@
 import React from 'react';
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    TouchableHighlight,
-} from 'react-native';
- 
-class PhotoCapture extends React.Component {
+import { Text, Button, View} from 'react-native';
+
+class PhotoCaptureScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Timelapse'
+    };
+
     render() {
+        const { navigate } = this.props.navigation;
         return (
             <View>
-                <
+                <Text>Progress</Text>
+                <Button
+                    title="Done"
+                    onPress={() => {
+                        navigate('PhotoNotes')
+                    }}></Button>
             </View>
         );
     }
- 
-    _switchCamera() {
-        var state = this.state;
-        state.cameraType = state.cameraType === Camera.constants.Type.back ? Camera.constants.Type.front : Camera.constants.Type.back;
-        this.setState(state);
-    }
- 
-    _takePicture() {
-        this.refs.cam.capture(function(err, data) {
-            console.log(err, data);
-        });
-    }
 }
- 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "transparent",
-    },
-    buttonBar: {
-        flexDirection: "row",
-        position: "absolute",
-        bottom: 25,
-        right: 0,
-        left: 0,
-        justifyContent: "center"
-    },
-    button: {
-        padding: 10,
-        color: "#FFFFFF",
-        borderWidth: 1,
-        borderColor: "#FFFFFF",
-        margin: 5
-    },
-    buttonText: {
-        color: "#FFFFFF"
-    }
-});
- 
-export default PhotoCaptureScreen
+
+export default PhotoCaptureScreen;
